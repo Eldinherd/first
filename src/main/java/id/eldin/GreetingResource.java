ac8a70a742f2948b9205742303e152d82f6e1a63
Eldin Herdiana <eldinherdiana86@gmail.com>
2023-02-28 21:43:24 +0700

    change to world

--- src/main/java/id/eldin/GreetingResource.java
+++ src/main/java/id/eldin/GreetingResource.java
@@ -11,20 +11,6 @@ public class GreetingResource {
     public String get() {
         return "This is a sample GET endpoint";
     }
-    @POST
-    @Produces(MediaType.TEXT_PLAIN)
-    public String post(){
-        return "This is a sample POST endpoint ";
-    }
-    @PUT
-    @Produces(MediaType.TEXT_PLAIN)
-    public String put(){
-       return "This is a sample POST endpoint " ;
-    }
-    @DELETE
-    @Produces(MediaType.TEXT_PLAIN)
-    public String delet(){
-        return "This is a sample POST endpoint " ;
-    }
+
 
 }
