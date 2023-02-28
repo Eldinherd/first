c8cc6b6a61f1fb70b3de3880d90ae03f51853c75
Eldin Herdiana <eldinherdiana86@gmail.com>
2023-02-28 22:24:42 +0700

    new endpoint 1 kategori

--- src/main/java/id/eldin/GreetingResource.java
+++ src/main/java/id/eldin/GreetingResource.java
@@ -3,13 +3,26 @@ package id.eldin;
 import javax.ws.rs.*;
 import javax.ws.rs.core.MediaType;
 
-@Path("/sample")
+@Path("")
 public class GreetingResource {
 
     @GET
+    @Path("/info")
     @Produces(MediaType.TEXT_PLAIN)
-    public String get() {
-        return "This is a sample GET endpoint";
+    public String info() {
+        return "info : This is a sample GET endpoint";
+    }
+    @GET
+    @Path("/about")
+    @Produces(MediaType.TEXT_PLAIN)
+    public String about() {
+        return "about : This is a sample GET endpoint";
+    }
+    @GET
+    @Path("/home")
+    @Produces(MediaType.TEXT_PLAIN)
+    public String home() {
+        return "home : This is a sample GET endpoint";
     }
 
 
